86a932f625cde99e31ca68ada1a7d811a41c7a07
Wataru Noguchi <wnoguchi.0727@gmail.com>
2013-12-29 21:05:58 +0900

    Write Development Environment Vagrant provisioning.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,22 @@
 # Cookbook Name:: sakura-cloud-cli
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
+# Copyright (C) 2013 Wataru Noguchi
 # 
-# All rights reserved - Do Not Redistribute
+# All rights reserved
 #
+
+service "iptables" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :disable, :stop ]
+end
+
+execute "devtools" do
+  user "root"
+  command 'yum -y groupinstall "Development Tools"'
+  action :run
+end
+
+package "ruby" do
+  action :install
+end
